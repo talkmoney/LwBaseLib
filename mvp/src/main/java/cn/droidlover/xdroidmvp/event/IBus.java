5cd3a68ac28078f024cde9f523a7b81d83829abb
DESKTOP-HJVFBKL\admin <chenjianrun@talkmoney.cn>
2018-09-06 20:30:23 +0800

    first commit

---
+++ mvp/src/main/java/cn/droidlover/xdroidmvp/event/IBus.java
@@ -0,0 +1,18 @@
+package cn.droidlover.xdroidmvp.event;
+
+/**
+ * Created by wanglei on 2016/12/22.
+ */
+
+public interface IBus {
+
+    void register(Object object);
+    void unregister(Object object);
+    void post(IEvent event);
+    void postSticky(IEvent event);
+
+    interface IEvent {
+        int getTag();
+    }
+
+}
---
+++ mvp/src/main/java/cn/droidlover/xdroidmvp/utils/LwSharePreUtils.java
@@ -0,0 +1,15 @@
+package cn.droidlover.xdroidmvp.utils;
+
+import com.blankj.utilcode.util.SPUtils;
+import com.blankj.utilcode.util.SpanUtils;
+
+/**
+ * Created by runla on 2018/8/17.
+ * 文件描述：
+ */
+
+public class LwSharePreUtils {
+    public LwSharePreUtils(){
+        SPUtils.getInstance().getAll();
+    }
+}
